bcd747d14d11d4f0e4535d33d6b5ea59d9da74bf
boomafromboomiland <lubomira.kovacova@avon.com>
2022-03-29 18:48:14 +0200

    Práca počas hodiny

--- 01-majitelka-vozu/app.js
+++ 01-majitelka-vozu/app.js
@@ -1,10 +1,21 @@
 // Vytvoř objekt auto, který bude mít vlastnosti: znacka, barva, rokVyroby,..
-
+let auto = {
+  znacka: 'ford',
+  farba: 'modrá',
+  rokVyroby: 2015,
+  spz: '1B2 3456',
+};
 
 // Přidej novou vlastnost (majitel a prirad ji svoje jmeno jako hodnotu)
 
+auto.majitel = {};
+auto.majitel.meno = 'Ľubka';
+auto.majitel.priezvisko = 'Kováčová';
 
 // Přepiš značku na Škoda
-
+auto['znacka'] = 'Škoda';
 
 // Vytvor tri promenne majitelka, znacka a spz a vloz do techto elementu text z objektu auto.
+let majitelka = auto.majitel.meno + '' + auto.majitel.priezvisko;
+let znacka = auto.znacka; 
+let spz = auto.spz;
